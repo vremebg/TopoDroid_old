134597e2281495afc1a73edafd4822b8123d35f2
Stanislav Asenov <stanislav2003@abv.bg>
2018-05-31 12:49:35 +0300

    added cabeja DXF lib

--- app/src/main/java/com/topodroid/DistoX/SketchDxf.java
+++ app/src/main/java/com/topodroid/DistoX/SketchDxf.java
@@ -20,6 +20,8 @@ import java.util.ArrayList;
 // import java.util.HashMap;
 
 import android.util.SparseArray;
+import org.kabeja.DraftDocument;
+import org.kabeja.dxf.generator.DXFGenerator;
 
 class SketchDxf
 {
@@ -28,6 +30,8 @@ class SketchDxf
 
   static void write( PrintWriter out, String fullname, SketchModel model )
   {
+    DraftDocument doc= new DraftDocument();
+    DXFGenerator generator = new DXFGenerator();
     DistoXNum num = model.mNum;
     // header
     out.printf("999\nDXF created from TopoDroid\n");
